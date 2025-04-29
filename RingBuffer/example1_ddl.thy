7b3f571bf9b51b5da29201f8b3dcdb018c3d0821
laila-fangyan <laila_fangyan@yahoo.com>
2025-04-29 16:09:15 +0100

    update examples for testing isabelleconnector

--- RingBuffer/example1_ddl.thy
+++ RingBuffer/example1_ddl.thy
@@ -244,14 +244,36 @@ lemma Trans_stm0_core'_ddlf:
 
   (* Rewrite the goal to allow multiple events *)
   apply (simp add: one_step_ahead_GlobalNdet_iterations'_FD_iff_GlobalNdet_iterations_FD[THEN sym] )
+    (* Simplify away the events in the cases not inclucing interrupt *)
+  apply (auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
+ generalized_refine_guarded_extchoice_star eat_lemma no_step_refine generalized_refine_guarded_extchoice write_proving_Mndetprefix_UNIV_ref GlobalNdet_refine_no_step )
 
-  (* Simplify away the events in the cases not inclucing interrupt *)
-   apply (auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
+  (* the below is simpler than above line, and works. the above is used in ex1_ddlf_no_final *)
+ (*  apply (auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
   eat_lemma no_step_refine generalized_refine_guarded_extchoice write_proving_Mndetprefix_UNIV_ref GlobalNdet_refine_no_step )
-
+*)
+  sledgehammer
 (*the guard can not be discharged*)
   nitpick
   oops
+
+method deadlock_free' uses P_def assms=
+  (rule df_step_param_intro[OF P_def]
+, simp add: bi_extchoice_norm  biextchoic_normalization  biextchoic_normalization_nguard_prefix read_Seq write_Seq write0_Seq
+  , simp add: one_step_ahead_GlobalNdet_iterations'_FD_iff_GlobalNdet_iterations_FD[THEN sym]
+, auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
+ generalized_refine_guarded_extchoice_star eat_lemma no_step_refine generalized_refine_guarded_extchoice write_proving_Mndetprefix_UNIV_ref GlobalNdet_refine_no_step
+, insert NIDS_stm0.exhaust atLeast0AtMost, auto
+, (simp add: SSTOP_nonTerm  prefix_Skip_no_initial_tick non_terminating_Interrupt_Seq write0_Seq GlobalNdet_refine_no_step SSTOP_refine eat_lemma iso_tuple_UNIV_I)+
+)
+
+
+
+lemma Trans_stm0_core'_ddlf_auto:
+   \<open>deadlock_free (\<sqinter> n \<in> UNIV. Trans_stm0_core'\<cdot>n)\<close>
+  by (deadlock_free' P_def: Trans_stm0_core'.simps)
+
+
 
 end
 thm Trans.Trans_stm0_core_def
---
+++ RingBuffer/example1_ddl_isabelleconnector_test.thy
@@ -0,0 +1,45 @@
+theory example1_ddl_isabelleconnector_test 
+	imports "HOLCF-Library.Nat_Discrete" "HOLCF-Library.Int_Discrete"
+          "HOLCF-Library.List_Cpo"  DeadlockFreedom_Automation Law_Interrupt_Seq  example1_ddl (*Example1_ddlf_no_final can not be imported, why?*)
+begin
+
+lemma Trans_stm0_core'_ddlf:
+  \<open>deadlock_free (\<sqinter> n \<in> UNIV. Trans.Trans_stm0_core'\<cdot>n)  \<close>
+  (* Apply induction *)
+  apply (rule df_step_param_intro[OF Trans.Trans_stm0_core'.simps])
+  (* Normalisation *)
+  apply (simp add: bi_extchoice_norm  biextchoic_normalization  biextchoic_normalization_nguard_prefix read_Seq write_Seq write0_Seq)
+
+  (* Rewrite the goal to allow multiple events *)
+  apply (simp add: one_step_ahead_GlobalNdet_iterations'_FD_iff_GlobalNdet_iterations_FD[THEN sym] )
+    (* Simplify away the events in the cases not inclucing interrupt *)
+  apply (auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
+ generalized_refine_guarded_extchoice_star eat_lemma no_step_refine generalized_refine_guarded_extchoice write_proving_Mndetprefix_UNIV_ref GlobalNdet_refine_no_step )
+
+  (* the below is simpler than above line, and works. the above is used in ex1_ddlf_no_final *)
+ (*  apply (auto intro!:prefix_proving_Mndetprefix_UNIV_ref(3)
+  eat_lemma no_step_refine generalized_refine_guarded_extchoice write_proving_Mndetprefix_UNIV_ref GlobalNdet_refine_no_step )
+*)
+ 
+(*the guard can not be discharged*)
+  nitpick
+  sorry
+
+(*
+lemma Trans_stm0_core'_ddlf_auto:
+ \<open>deadlock_free (\<sqinter> n \<in> UNIV. Trans.Trans_stm0_core'\<cdot>n)\<close>
+  by (Trans.deadlock_free' P_def: Trans.Trans_stm0_core'.simps)
+*)
+(*is there a way to nitpick failed proof method?*)
+
+
+
+
+
+
+end
+
+end
+
+
+
